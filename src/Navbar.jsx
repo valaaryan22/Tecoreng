5c328076ff4df44db5477a5a291320a81690571f
valaaryan22 <valaaryan22@gmail.com>
2025-01-24 15:32:30 +0530

    nasir add 2

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import  { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from "./logo.svg";
 import laptop from './laptop.svg';
